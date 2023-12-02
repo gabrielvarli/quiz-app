cf12acf35e1af1d0b3ed821dee703b59b65686f7
gabrielvarli <gabriel.varli@hotmail.com>
2023-12-02 21:53:33 +0100

    grundläggande kod

---
+++ script.js
@@ -0,0 +1,43 @@
+const questions = [
+    {
+        questions: 'Vad är Sveriges Huvudstad ?' ,
+        answers: [
+            {text: 'Göteborg', correct: false},
+            {text: 'Stockholm', correct: true},
+            {text: 'Malmö', correct: false},
+            {text: 'Örebro', correct: false},
+        ]
+    },
+    {
+        questions: 'Vad är Sveriges Huvudstad ?' ,
+        answers: [
+            {text: 'Göteborg', correct: false},
+            {text: 'Stockholm', correct: true},
+            {text: 'Malmö', correct: false},
+            {text: 'Örebro', correct: false},
+        ]
+    },
+    {
+        questions: 'Vad är Sveriges Huvudstad ?' ,
+        answers: [
+            {text: 'Göteborg', correct: false},
+            {text: 'Stockholm', correct: true},
+            {text: 'Malmö', correct: false},
+            {text: 'Örebro', correct: false},
+        ]
+    },
+    {
+        questions: 'Vad är Sveriges Huvudstad ?' ,
+        answers: [
+            {text: 'Göteborg', correct: false},
+            {text: 'Stockholm', correct: true},
+            {text: 'Malmö', correct: false},
+            {text: 'Örebro', correct: false},
+        ]
+    },
+]
+
+const questionElement = document.getElementById('question');
+const anserButton = document.getElementById('answer-buttons');
+const nextButton = document.getElementById('next-btn');
+
