0db8f9ed0cba25920097d2d93252dc2d585b89e8
gabrielvarli <gabriel.varli@hotmail.com>
2023-12-07 22:31:49 +0100

    quiz funkar

--- script.js
+++ script.js
@@ -27,6 +27,60 @@ const questions = [
         ]
     },
     {
+        question: 'Vem är Sveries genom tiderna bästa fotbollsspelare ?' ,
+        answers: [
+            {text: 'Henrik Larsson', correct: false},
+            {text: 'Zlatan Ibrahimovic', correct: true},
+            {text: 'Martin Mutumba', correct: false},
+            {text: 'John Gudetti', correct: false},
+        ]
+    },
+    {
+        question: 'Hur många invånare bor det i Sverige ?' ,
+        answers: [
+            {text: '8 miljoner', correct: false},
+            {text: '10 miljoner', correct: true},
+            {text: '9 miljoner', correct: false},
+            {text: '11 miljoner', correct: false},
+        ]
+    },
+    {
+        question: 'Hur många invånare bor det i Sverige ?' ,
+        answers: [
+            {text: '8 miljoner', correct: false},
+            {text: '10 miljoner', correct: true},
+            {text: '9 miljoner', correct: false},
+            {text: '11 miljoner', correct: false},
+        ]
+    },
+    {
+        question: 'Hur många invånare bor det i Sverige ?' ,
+        answers: [
+            {text: '8 miljoner', correct: false},
+            {text: '10 miljoner', correct: true},
+            {text: '9 miljoner', correct: false},
+            {text: '11 miljoner', correct: false},
+        ]
+    },
+    {
+        question: 'Hur många invånare bor det i Sverige ?' ,
+        answers: [
+            {text: '8 miljoner', correct: false},
+            {text: '10 miljoner', correct: true},
+            {text: '9 miljoner', correct: false},
+            {text: '11 miljoner', correct: false},
+        ]
+    },
+    {
+        question: 'Hur många invånare bor det i Sverige ?' ,
+        answers: [
+            {text: '8 miljoner', correct: false},
+            {text: '10 miljoner', correct: true},
+            {text: '9 miljoner', correct: false},
+            {text: '11 miljoner', correct: false},
+        ]
+    },
+    {
         question: 'Hur många invånare bor det i Sverige ?' ,
         answers: [
             {text: '8 miljoner', correct: false},
@@ -37,112 +91,84 @@ const questions = [
     },
 ]
 
-const questionElement = document.getElementById('question');
-const answerButtons = document.getElementById('answer-buttons');
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById('next-btn');
 
-// vad gör denna kod ?
 let currentQuestionIndex = 0;
 let score = 0;
 
-// vad gör denna ?
-function startQuiz(){
+function startQuiz() {
     currentQuestionIndex = 0;
     score = 0;
-    nextButton.innerHTML = "Next";
+    nextButton.innerHTML = "next";
     showQuestion();
 }
-
-// antar att fråge funktioner körs här 
-function showQuestion(){
+function showQuestion() {
     resetState();
     let currentQuestion = questions[currentQuestionIndex];
     let questionNo = currentQuestionIndex + 1;
-    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+    questionElement.innerHTML = questionNo + ". " + currentQuestionIndex.question;
 
-    // antar att svar funktionen körs här 
     currentQuestion.answers.forEach(answer => {
-        const button = document.createElement('button');
+        const button = document.createElement("button");
         button.innerHTML = answer.text;
-        button.classList.add('btn');
+        button.classList.add("btn");
         answerButtons.appendChild(button);
-    })
+        if(answer.correct){
+            button.dataset.correct = answer.correct; 
+        }
+        button.addEventListener("click",selectAnswer);
+    }) 
 }
 
 function resetState(){
-    nextButton.style.display = "none"; 
-     while(answerButtons.firstChild){
-        answerButtons.removeChild(answerButton.firstChild)
-     }
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+function selectAnswer(e) {
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    }else{
+        selectedBtn.classList.add("incorrect");
+    }
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true") {
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+
 }
 
+function showScore(){
+    resetState();
+    questionElement.innerHTML = `You scored ${score} out of ${questions.length}!`;
+    nextButton.innerHTML = "Play Again";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton () {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length){
+        showQuestion();
+    }else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    } else (
+        startQuiz()
+    )
+})
+
 startQuiz();
-
-
-// darkmode func
-
-function calculateSettingAsThemeString({ localStorageTheme, systemSettingDark }) {
-    if (localStorageTheme !== null) {
-      return localStorageTheme;
-    }
-  
-    if (systemSettingDark.matches) {
-      return "dark";
-    }
-  
-    return "light";
-  }
-  
-  /**
-  * Utility function to update the button text and aria-label.
-  */
-  function updateButton({ buttonEl, isDark }) {
-    const newCta = isDark ? "Change to light theme" : "Change to dark theme";
-    // use an aria-label if you are omitting text on the button
-    // and using a sun/moon icon, for example
-    buttonEl.setAttribute("aria-label", newCta);
-    buttonEl.innerText = newCta;
-  }
-  
-  /**
-  * Utility function to update the theme setting on the html tag
-  */
-  function updateThemeOnHtmlEl({ theme }) {
-    document.querySelector("html").setAttribute("data-theme", theme);
-  }
-  
-  
-  /**
-  * On page load:
-  */
-  
-  /**
-  * 1. Grab what we need from the DOM and system settings on page load
-  */
-  const button = document.querySelector("[data-theme-toggle]");
-  const localStorageTheme = localStorage.getItem("theme");
-  const systemSettingDark = window.matchMedia("(prefers-color-scheme: dark)");
-  
-  /**
-  * 2. Work out the current site settings
-  */
-  let currentThemeSetting = calculateSettingAsThemeString({ localStorageTheme, systemSettingDark });
-  
-  /**
-  * 3. Update the theme setting and button text accoridng to current settings
-  */
-  updateButton({ buttonEl: button, isDark: currentThemeSetting === "dark" });
-  updateThemeOnHtmlEl({ theme: currentThemeSetting });
-  
-  /**
-  * 4. Add an event listener to toggle the theme
-  */
-  button.addEventListener("click", (event) => {
-    const newTheme = currentThemeSetting === "dark" ? "light" : "dark";
-  
-    localStorage.setItem("theme", newTheme);
-    updateButton({ buttonEl: button, isDark: newTheme === "dark" });
-    updateThemeOnHtmlEl({ theme: newTheme });
-  
-    currentThemeSetting = newTheme;
-  }); 
